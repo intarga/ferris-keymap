40686bae90ad21ac797d3d0fa81781b3f7615b81
Ingrid <git@ingrids.email>
2022-07-28 15:57:20 +0200

    add dedicated shift back in on raise layer

--- keymap.c
+++ keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_0,    KC_1,    KC_2,    KC_3,    KC_GRV,          _______, KC_LBRC, KC_RBRC, KC_INS,  KC_ESC,
     KC_MINS, KC_4,    KC_5,    KC_6,    KC_QUOT,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,
     KC_EQL,  KC_7,    KC_8,    KC_9,    KC_BSLS,         _______, KC_LPRN, KC_RPRN, _______, KC_ENT,
-                                    _______, _______, _______, _______
+                                    KC_LSFT, _______, _______, _______
   )
 };
 
