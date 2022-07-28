f2a55b17f7c1d74d4f43177a7bbea85f6dd238a4
Ingrid <git@ingrids.email>
2022-07-28 14:49:42 +0200

    reduce tapping term

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 300
+#define TAPPING_TERM 250
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
