732d66ca4d23fd0c1ed0f89598ad81f72024e7fd
Ingrid <git@ingrids.email>
2022-07-30 15:41:56 +0200

    add adjust layer, more rearrangement

--- keymap.c
+++ keymap.c
@@ -3,7 +3,8 @@
 enum ferris_layers {
   _COLEMAK,
   _LOWER,
-  _RAISE
+  _RAISE,
+  _ADJUST
 };
 
 // Left-hand home row mods
@@ -32,27 +33,38 @@ enum ferris_layers {
 #define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_COLEMAK] = LAYOUT( /* COLEMAK */
+  [_COLEMAK] = LAYOUT(
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,            KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
     KC_A,    HOME_R,  HOME_S,  HOME_T,  KC_D,            KC_H,    HOME_N,  HOME_E,  HOME_I,  KC_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                     KC_LSFT, KC_SPC,  RAISE,   LOWER
-  ) ,
+  ),
 
-  [_LOWER] = LAYOUT( /* [> LOWER <] */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,         KC_0,    KC_1,    KC_2,    KC_3,    KC_GRV,
-    KC_F5,   KC_F6,   KC_F7,   KC_F8,   QK_BOOT,         KC_MINS, KC_4,    KC_5,    KC_6,    KC_QUOT,
-    KC_F9,   KC_F10,  KC_F11,  KC_F12,  DEBUG,           KC_EQL,  KC_7,    KC_8,    KC_9,    KC_BSLS,
+  [_LOWER] = LAYOUT(
+    _______, _______, _______, _______, NO_AA,           _______, CTRL_D,  CTRL_U,  _______, _______,
+    KC_DEL,  KC_ESC,  KC_TAB,  KC_ENT,  NO_AE,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    _______, _______, _______, _______, NO_OE,           _______, KC_PGDN, KC_PGUP, _______, _______,
+                                    _______, KC_BSPC, _______, _______
+  ),
+
+  [_RAISE] = LAYOUT(
+    _______, _______, KC_LBRC, KC_RBRC, KC_GRV,          KC_0,    KC_1,    KC_2,    KC_3,    KC_BSPC,
+    KC_LABK, KC_RABK, KC_LPRN, KC_RPRN, KC_QUOT,         KC_MINS, KC_4,    KC_5,    KC_6,    KC_PLUS,
+    _______, _______, KC_LCBR, KC_RCBR, KC_BSLS,         KC_EQL,  KC_7,    KC_8,    KC_9,    KC_ENT,
                                     _______, _______, _______, _______
   ),
 
-  [_RAISE] = LAYOUT( /* [> RAISE <] */
-    KC_MRWD, KC_MFFD, KC_LBRC, KC_RBRC, NO_AA,           KC_VOLU, CTRL_D,  CTRL_U,  KC_BRIU, KC_ESC,
-    KC_BSPC, KC_INS,  KC_LPRN, KC_RPRN, NO_AE,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,
-    KC_MUTE, KC_MPLY, KC_LCBR, KC_RCBR, NO_OE,           KC_VOLD, KC_PGDN, KC_PGUP, KC_BRID, KC_ENT,
+  [_ADJUST] = LAYOUT(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,          _______, _______, _______, _______, _______,
+    KC_F5,   KC_F6,   KC_F7,   KC_F8,   QK_BOOT,         _______, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU,
+    KC_F9,   KC_F10,  KC_F11,  KC_F12,  DEBUG,           _______, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE,
                                     _______, _______, _______, _______
   )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 // Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
