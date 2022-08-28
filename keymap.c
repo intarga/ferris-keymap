964ef99381a964e14a36c799d0fe78fc9511dc3c
Ingrid <git@ingrids.email>
2022-08-28 19:57:36 +0200

    add beakl layer

--- keymap.c
+++ keymap.c
@@ -2,22 +2,27 @@
 
 enum ferris_layers {
   _COLEMAK,
+  _BEAKL,
   _LOWER,
   _RAISE,
   _ADJUST
 };
 
 // Left-hand home row mods
-// #define HOME_A LSFT_T(KC_A)
 #define HOME_R LCTL_T(KC_R)
 #define HOME_S LALT_T(KC_S)
 #define HOME_T LGUI_T(KC_T)
+#define BEAK_I LCTL_T(KC_I)
+#define BEAK_E LALT_T(KC_E)
+#define BEAK_A LGUI_T(KC_A)
 
 // Right-hand home row mods
 #define HOME_N RGUI_T(KC_N)
 #define HOME_E LALT_T(KC_E)
 #define HOME_I RCTL_T(KC_I)
-// #define HOME_O RSFT_T(KC_O)
+#define BEAK_S RGUI_T(KC_S)
+#define BEAK_T LALT_T(KC_T)
+#define BEAK_N RCTL_T(KC_N)
 
 // Norwegian letters
 #define NO_AE LALT(KC_QUOT)
@@ -31,12 +36,21 @@ enum ferris_layers {
 // Layer keys
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define COLEMAK DF(_COLEMAK)
+#define BEAKL DF(_BEAKL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,            KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
     KC_A,    HOME_R,  HOME_S,  HOME_T,  KC_D,            KC_H,    HOME_N,  HOME_E,  HOME_I,  KC_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                                    KC_LSFT, KC_SPC,  RAISE,   LOWER
+  ),
+
+  [_BEAKL] = LAYOUT(
+    KC_Q,    KC_H,    KC_O,    KC_U,    KC_X,            KC_G,    KC_C,    KC_M,    KC_R,    KC_V,
+    KC_Y,    BEAK_I,  BEAK_E,  BEAK_A,  KC_DOT,          KC_D,    BEAK_S,  BEAK_T,  BEAK_N,  KC_B,
+    KC_J,    KC_SLSH, KC_COMM, KC_K,    KC_QUOT,         KC_W,    KC_F,    KC_L,    KC_P,    KC_Z,
                                     KC_LSFT, KC_SPC,  RAISE,   LOWER
   ),
 
@@ -55,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ADJUST] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,          _______, _______, _______, _______, _______,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,          _______, COLEMAK, BEAKL,   _______, _______,
     KC_F5,   KC_F6,   KC_F7,   KC_F8,   QK_BOOT,         _______, _______, _______, _______, _______,
     KC_F9,   KC_F10,  KC_F11,  KC_F12,  DEBUG,           _______, _______, _______, _______, _______,
                                     _______, _______, _______, _______
